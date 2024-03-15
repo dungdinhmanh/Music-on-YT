729840878a7ae50f7faa916a214648ba04ef52c2
dungdinhmanh <dungdinhmanh0209@gmail.com>
2024-03-15 21:16:44 +0700

    update

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from ytmusicapi import YTMusic
 from flet import *
-
+#from pytube import YouTube as yt
---
+++ pages/login.py
@@ -0,0 +1,5 @@
+import flet as ft
+import os
+from flet.auth.providers.google_oauth_provider import GoogleOAuthProvider
+def main(page:Page):
+    page.theme_mode=""
